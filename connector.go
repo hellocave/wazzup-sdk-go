9fb00a0cf712cbb55c93b1160a83e55e91ba615b
Tom Koele <tom@codeandcoding.com>
2019-10-19 01:37:52 +0200

    added custom caller option

--- connector.go
+++ connector.go
@@ -19,6 +19,12 @@ var services = map[string]string{
 	"output":   "OutputService",
 }
 
+// Caller is a handler for calling http endpoints
+type Caller interface {
+	Call(URL string) ([]byte, int, error)
+	CallPost(URL string, data []byte) ([]byte, int, error)
+}
+
 // Response represents a Wazzup API response
 type Response struct {
 	XMLName   xml.Name    `xml:"Result"`
@@ -31,13 +37,14 @@ type Response struct {
 
 // Connector contains a connection to Wazzup
 type Connector struct {
-	token string
+	token  string
+	caller Caller
 	sync.RWMutex
 }
 
 // NewConnector returns a new Connector instance
-func NewConnector(token string) *Connector {
-	return &Connector{token: token}
+func NewConnector(token string, callHandler Caller) *Connector {
+	return &Connector{token: token, caller: callHandler}
 }
 
 // call a GET endpoint for a given service
@@ -47,10 +54,16 @@ func (c *Connector) callGet(endpoint string, service string) (*Response, string,
 		return nil, "", err
 	}
 
+	if c.caller != nil {
+		res, err := c.getCaller(url)
+		return res, url, err
+	}
+
 	r, err := http.Get(url)
 	if err != nil {
 		return nil, "", fmt.Errorf("fetch error: %s", err)
 	}
+	defer r.Body.Close()
 
 	res, err := parseResponse(r.Body)
 	if err != nil {
@@ -60,6 +73,22 @@ func (c *Connector) callGet(endpoint string, service string) (*Response, string,
 	return res, url, nil
 }
 
+// call a GET endpoint using the given handler
+func (c *Connector) getCaller(endpoint string) (*Response, error) {
+	byt, status, err := c.caller.Call(endpoint)
+	if err != nil {
+		return &Response{}, err
+	}
+
+	if status > 299 {
+		return &Response{}, fmt.Errorf("call error, status %d", status)
+	}
+
+	r := bytes.NewBuffer(byt)
+
+	return parseResponse(r)
+}
+
 // call a POST endpoint for a given service
 func (c *Connector) callPost(endpoint string, service string, data []byte) (*Response, error) {
 	url, err := parseURL(endpoint, service, c.token)
@@ -67,12 +96,33 @@ func (c *Connector) callPost(endpoint string, service string, data []byte) (*Res
 		return nil, err
 	}
 
+	if c.caller != nil {
+		return c.postCaller(url, data)
+	}
+
 	r, err := http.Post(url, "text/html", bytes.NewBuffer(data))
 	if err != nil {
 		return nil, fmt.Errorf("fetch error: %s", err)
 	}
+	defer r.Body.Close()
 
 	return parseResponse(r.Body)
+}
+
+// call a POST endpoint using the given handler
+func (c *Connector) postCaller(endpoint string, data []byte) (*Response, error) {
+	byt, status, err := c.caller.CallPost(endpoint, data)
+	if err != nil {
+		return &Response{}, err
+	}
+
+	if status > 299 {
+		return &Response{}, fmt.Errorf("call error, status %d", status)
+	}
+
+	r := bytes.NewBuffer(byt)
+
+	return parseResponse(r)
 }
 
 func parseURL(endpoint string, service string, token string) (string, error) {
@@ -89,9 +139,7 @@ func parseURL(endpoint string, service string, token string) (string, error) {
 		stripSlashes(baseURL), serv, token, stripSlashes(endpoint)), nil
 }
 
-func parseResponse(body io.ReadCloser) (*Response, error) {
-	defer body.Close()
-
+func parseResponse(body io.Reader) (*Response, error) {
 	bts, err := ioutil.ReadAll(body)
 	if err != nil {
 		return nil, fmt.Errorf("read error: %s", err)
